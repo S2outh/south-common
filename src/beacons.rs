9ca6ebbd1e03a7973f243ab22e3aee827f06748a
eleocraft <eliseo.cailloux@gmx.de>
2025-12-17 18:50:04 +0100

    bumping tmtc-system version
    this introduces the new beacon

--- src/beacons.rs
+++ src/beacons.rs
@@ -1,7 +1,7 @@
 use tmtc_system::*;
 
 beacon!(LowRateTelemetry, crate::definitions::telemetry,
-    header(0x4E, 0x4A, 1, 0, 37, 0, 0),
+    id = 0,
     telemetry(
         Timestamp, 
         lst::Uptime,
@@ -15,7 +15,7 @@ beacon!(LowRateTelemetry, crate::definitions::telemetry,
 );
 
 beacon!(MidRateTelemetry, crate::definitions::telemetry,
-    header(0x4E, 0x4A, 2, 0, 28, 0, 0),
+    id = 1,
     telemetry(
         Timestamp, 
         eps::EnableBitmap,
