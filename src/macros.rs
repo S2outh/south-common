cb7e58f6081140a5d5ead914e0ea01abd73bda0d
eleocraft <eliseo.cailloux@gmx.de>
2026-05-02 21:00:36 +0200

    fixing other macro opts

--- src/macros.rs
+++ src/macros.rs
@@ -1,24 +1,25 @@
 #[macro_export]
 macro_rules! gen_obdh_types {
-    ($chell_union: ty, $name: ident, $on_tm: ty, $subsys_cmd: ty) => {
+    ($name: ident, $def_path: path, $on_tm: ty, $subsys_cmd: ty) => {
         $crate::paste::paste! {
-            type [<$name ComChannels>] = $crate::obdh::InternalComChannels<$subsys_cmd, {$chell_union::SIZE}>;
-            type [<$name CanReceiver>] = $crate::obdh::SouthCanReceiver<'static, $subsys_cmd, {$chell_union::SIZE}, $on_tm>;
-            type [<$name CanSender>] = $crate::obdh::SouthCanSender<'static, $subsys_cmd, {$chell_union::SIZE}>;
-            type [<$name TMSender>] = $crate::obdh::TMSender<'static, {$chell_union::SIZE}>;
+            type [<$name ChellUnion>] = $crate::chell::fd_compat_chell_union!($def_path);
+            type [<$name ComChannels>] = $crate::obdh::InternalComChannels<$subsys_cmd, {[<$name ChellUnion>]::SIZE}>;
+            type [<$name CanReceiver>] = $crate::obdh::SouthCanReceiver<'static, $subsys_cmd, {[<$name ChellUnion>]::SIZE}, $on_tm>;
+            type [<$name CanSender>] = $crate::obdh::SouthCanSender<'static, $subsys_cmd, {[<$name ChellUnion>]::SIZE}>;
+            type [<$name TMSender>] = $crate::obdh::TMSender<'static, {[<$name ChellUnion>]::SIZE}>;
             type [<$name TCReceiver>] = $crate::obdh::TCReceiver<'static, $subsys_cmd>;
         }
     };
 
-    ($chell_union: ty, $name: ident, cmd => $subsys_cmd: ty) => {
-        $crate::gen_obdh_types!($chell_union, $name, $crate::obdh::EmptyFunc, $subsys_cmd)
+    ($name: ident, $def_path: path, cmd => $subsys_cmd: ty) => {
+        $crate::gen_obdh_types!($name, $def_path, $crate::obdh::EmptyFunc, $subsys_cmd);
     };
 
-    ($chell_union: ty, $name: ident, on_tm => $on_tm: ty) => {
-        $crate::gen_obdh_types!($chell_union, $name, $on_tm, $crate::types::NoCommand)
+    ($name: ident, $def_path: path, on_tm => $on_tm: ty) => {
+        $crate::gen_obdh_types!($name, $def_path, $on_tm, $crate::types::NoCommand);
     };
 
-    ($chell_union: ty, $name: ident) => {
-        $crate::gen_obdh_types!($chell_union, $name, $crate::obdh::EmptyFunc, $crate::types::NoCommand)
+    ($name: ident, $def_path: path) => {
+        $crate::gen_obdh_types!($name, $def_path, $crate::obdh::EmptyFunc, $crate::types::NoCommand);
     };
 }
