66d141b6de72e300fa787eef0f5d47cd09fe68c4
eleocraft <eliseo.cailloux@gmx.de>
2026-07-16 20:01:24 +0200

    removing lst tm fields that make no sense on air

--- src/definitions.rs
+++ src/definitions.rs
@@ -26,23 +26,8 @@ pub mod telemetry {
         #[chv(u32)]
         struct Uptime;
 
-        #[chv(i8)]
-        struct Rssi;
-
-        #[chv(u8)]
-        struct Lqi;
-
         #[chv(u32)]
         struct PacketsSent;
-
-        #[chv(u32)]
-        struct PacketsGood;
-
-        #[chv(u32)]
-        struct PacketsRejectedChecksum;
-
-        #[chv(u32)]
-        struct PacketsRejectedOther;
     }
 
     mod eps {
